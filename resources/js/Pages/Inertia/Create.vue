d75f27a75e6f4ebff1e118deeb2e03512642fab3
tomo <rhbgp7kt@gmail.com>
2024-06-16 02:22:24 +0900

    create

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+
+const form = reactive({
+    title: null,
+    content: null
+})
+
+const submitFunction = () => {
+    Inertia.post('/inertia', form)
+}
+
+</script>
+
+<template>
+    <form @submit.prevent="submitFunction">
+        <input name="title" v-model="form.title"><br>
+        <input name="content" v-model="form.content">
+        <button>送信</button>
+    </form>
+</template>
